f6688cfd0a98939650c2fccdf59caa58a2fcf90d
MrHertz <rus.syndicate@gmail.com>
2017-03-15 17:10:15 +0300

    reorganization

--- inc/Bd.class.php
+++ inc/Bd.class.php
@@ -5,25 +5,25 @@
 	$p (array) - сами параметры
  */
 class Sql {
-		private $sql, $stmt;
+	private $sql, $stmt;
     function __construct() {
-        $sql = new mysqli($Config::dbhost, $Config::dbuser, $Config::dbpassword, $Config::db);
+    	$sql = new mysqli($Config::dbhost, $Config::dbuser, $Config::dbpassword, $Config::db);
         if ($sql->connect_error) die('Connect Error (' . $sql->connect_errno . ') ' . $sql->connect_error);
     }
 
-		public function prepare($q) {
+	public function prepare($q) {
         $this->stmt = $this->sql->stmt_init();
         if ($this->stmt->prepare($q) === false)
-        		return false;
+			return false;
         else
-        		return true;
+			return true;
     }
 
-		public function query() {
-				if (($this->stmt->execute() === false)
-						or (($result = $this->stmt->get_result()) === false)
-								or ($this->stmt->close() === false))
-										die('Query Error [' . $this->stmt->errno . '] ' . $this->stmt->error);
+	public function query() {
+		if (($this->stmt->execute() === false)
+			or (($result = $this->stmt->get_result()) === false)
+				or ($this->stmt->close() === false))
+					die('Query Error [' . $this->stmt->errno . '] ' . $this->stmt->error);
         else
             return $result;
 		}
